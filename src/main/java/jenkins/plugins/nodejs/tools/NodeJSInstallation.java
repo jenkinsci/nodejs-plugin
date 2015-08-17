46ba3085df7adaefb6798599397ff6054dd32540
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-08-17 10:31:37 +0200

    use a SimpleBuildWrapper for workflow support as well as making codebase simpler

--- src/main/java/jenkins/plugins/nodejs/tools/NodeJSInstallation.java
+++ src/main/java/jenkins/plugins/nodejs/tools/NodeJSInstallation.java
@@ -1,15 +1,18 @@
 package jenkins.plugins.nodejs.tools;
 
 import hudson.*;
+import hudson.model.Computer;
 import hudson.model.EnvironmentSpecific;
 import hudson.model.Node;
 import hudson.model.TaskListener;
+import hudson.slaves.SlaveComputer;
 import jenkins.plugins.nodejs.NodeJSPlugin;
 import hudson.remoting.Callable;
 import hudson.slaves.NodeSpecific;
 import hudson.tools.ToolDescriptor;
 import hudson.tools.ToolInstallation;
 import hudson.tools.ToolProperty;
+import jenkins.security.MasterToSlaveCallable;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.File;
@@ -61,7 +64,9 @@ public class NodeJSInstallation extends ToolInstallation
     }
 
     public String getExecutable(Launcher launcher) throws InterruptedException, IOException {
-        return launcher.getChannel().call(new Callable<String, IOException>() {
+        return launcher.getChannel().call(new MasterToSlaveCallable<String, IOException>() {
+            private static final long serialVersionUID = 1L;
+
             public String call() throws IOException {
                 File exe = getExeFile();
                 if (exe.exists()) {
@@ -76,6 +81,16 @@ public class NodeJSInstallation extends ToolInstallation
         String execName = (Functions.isWindows()) ? WINDOWS_NODEJS_COMMAND : UNIX_NODEJS_COMMAND;
         String nodeJSHome = Util.replaceMacro(this.nodeJSHome, EnvVars.masterEnvVars);
         return new File(nodeJSHome, (Functions.isWindows() ? "":"bin/") + execName);
+    }
+
+    String getBinFolder() {
+        final Computer computer = Computer.currentComputer();
+
+        // TODO post 1.624 use Computer#isUnix
+        boolean unix = !Functions.isWindows();
+        if (computer instanceof SlaveComputer)
+            unix = ((SlaveComputer) computer).isUnix();
+        return unix ? getHome()+"/bin" : getHome();
     }
 
     @Extension
--- src/main/java/jenkins/plugins/nodejs/tools/NpmPackagesBuildWrapper.java
+++ src/main/java/jenkins/plugins/nodejs/tools/NpmPackagesBuildWrapper.java
@@ -1,42 +1,27 @@
 package jenkins.plugins.nodejs.tools;
 
-import com.google.common.base.Throwables;
 import hudson.*;
-import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.BuildListener;
 import hudson.model.Computer;
 import hudson.model.Run;
+import hudson.model.TaskListener;
 import jenkins.plugins.nodejs.NodeJSPlugin;
-import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
+import jenkins.tasks.SimpleBuildWrapper;
 import org.kohsuke.stapler.DataBoundConstructor;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
  * @author fcamblor
  */
-public class NpmPackagesBuildWrapper extends BuildWrapper {
+public class NpmPackagesBuildWrapper extends SimpleBuildWrapper {
 
-    private String nodeJSInstallationName;
+    private final String nodeJSInstallationName;
 
     @DataBoundConstructor
     public NpmPackagesBuildWrapper(String nodeJSInstallationName){
         this.nodeJSInstallationName = nodeJSInstallationName;
-    }
-
-    @Override
-    public Environment setUp(AbstractBuild build, Launcher launcher,
-            BuildListener listener) throws IOException, InterruptedException {
-        return new Environment(){
-            @Override
-            public boolean tearDown(AbstractBuild build, BuildListener listener)
-                    throws IOException, InterruptedException {
-                return true;
-            }
-        };
     }
 
     public String getNodeJSInstallationName() {
@@ -44,68 +29,23 @@ public class NpmPackagesBuildWrapper extends BuildWrapper {
     }
 
     @Override
-    public Launcher decorateLauncher(final AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException, Run.RunnerAbortedException {
-        return new DecoratedLauncher(launcher){
-            @Override
-            public Proc launch(ProcStarter starter) throws IOException {
-                // Avoiding potential NPE when calling starter.envs()
-                // Yes, this is weird...
-                String[] starterEnvs;
-                try {
-                   starterEnvs = starter.envs();
-                } catch (NullPointerException ex) {
-                    starterEnvs = new String[0];
-                }
+    public void setUp(Context context, Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener, EnvVars env) throws IOException, InterruptedException {
 
-                String pathSeparator = File.pathSeparator;
+        final Computer computer = Computer.currentComputer();
 
-                EnvVars vars = toEnvVars(starterEnvs);
+        NodeJSInstallation nodeJSInstallation =
+            NodeJSPlugin.instance().findInstallationByName(nodeJSInstallationName);
 
-                NodeJSInstallation nodeJSInstallation = 
-                    NodeJSPlugin.instance().findInstallationByName(nodeJSInstallationName);
+        nodeJSInstallation = nodeJSInstallation.forNode(computer.getNode(), listener);
+        nodeJSInstallation = nodeJSInstallation.forEnvironment(env);
+        String binFolder = nodeJSInstallation.getBinFolder();
 
-                try {
-                    nodeJSInstallation = nodeJSInstallation.forNode(build.getBuiltOn(), listener);
-                    nodeJSInstallation = nodeJSInstallation.forEnvironment(vars);
-
-                    Computer slave = Computer.currentComputer();
-                    String slavePathSeparator = (String)slave.getSystemProperties().get("path.separator");
-
-                    if (slavePathSeparator != null) {
-                        pathSeparator = slavePathSeparator;
-                    }
-                } catch (InterruptedException e) {
-                    Throwables.propagate(e);
-                }
-
-                // HACK: Avoids issue with invalid separators in EnvVars::override in case of different master/slave
-                
-                String overriddenPaths = NodeJSInstaller.binFolderOf(nodeJSInstallation, build.getBuiltOn())
-                        + pathSeparator
-                        + vars.get("PATH");
-                vars.override("PATH", overriddenPaths);
-
-                return super.launch(starter.envs(Util.mapToEnv(vars)));
-            }
-
-            private EnvVars toEnvVars(String[] envs) {
-                EnvVars vars = new EnvVars();
-                for (String line : envs) {
-                    vars.addLine(line);
-                }
-                return vars;
-            }
-        };
+        context.env("PATH+NODEJS", binFolder);
     }
 
+
     @Extension
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
-
     public static final class DescriptorImpl extends BuildWrapperDescriptor {
-
-        public DescriptorImpl() {
-            super(NpmPackagesBuildWrapper.class);
-        }
 
         @Override
         public boolean isApplicable(AbstractProject<?, ?> item) {
