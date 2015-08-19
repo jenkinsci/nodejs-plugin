4a6ef309ee9940c8e4362099e74bdeabbf2c8591
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-08-19 09:24:19 +0200

    remove obsolete code

--- src/main/java/jenkins/plugins/nodejs/tools/NodeJSInstaller.java
+++ src/main/java/jenkins/plugins/nodejs/tools/NodeJSInstaller.java
@@ -25,34 +25,24 @@ package jenkins.plugins.nodejs.tools;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
-
 import hudson.Extension;
 import hudson.FilePath;
-import hudson.Functions;
-import hudson.Util;
 import hudson.model.Node;
 import hudson.model.TaskListener;
-import hudson.os.PosixAPI;
-import hudson.slaves.NodeSpecific;
-import jenkins.MasterToSlaveFileCallable;
-import jenkins.plugins.tools.Installables;
-import hudson.remoting.Callable;
-import hudson.remoting.VirtualChannel;
 import hudson.tools.DownloadFromUrlInstaller;
 import hudson.tools.ToolInstallation;
 import hudson.util.ArgumentListBuilder;
-import hudson.util.jna.GNUCLibrary;
-
 import jenkins.security.MasterToSlaveCallable;
-import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nullable;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
