fd25f8e15848b2613d0bcb7c4debf616e55d8fe4
Nikolas Falco <nfalco79@hotmail.com>
2017-01-27 00:26:22 +0100

    Improve user configuration validation message. Add test case for PATH variable.

--- src/test/java/jenkins/plugins/nodejs/configfiles/NPMConfigTest.java
+++ src/test/java/jenkins/plugins/nodejs/configfiles/NPMConfigTest.java
@@ -3,12 +3,9 @@ package jenkins.plugins.nodejs.configfiles;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-
 import org.jenkinsci.lib.configprovider.model.Config;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import hudson.model.Descriptor;
@@ -18,8 +15,6 @@ public class NPMConfigTest {
 
     @Rule
     public JenkinsRule j = new JenkinsRule();
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void test_load_template() {
@@ -31,28 +26,6 @@ public class NPMConfigTest {
         Config config = provider.newConfig("testId");
         assertThat("Unexpected config class", config, instanceOf(NPMConfig.class));
         assertThat("Expected the default template, instead got empty", config.content, allOf(notNullValue(), is(not(""))));
-    }
-
-    @Test
-    public void test_new_config() {
-        String id = "test_id";
-        NPMConfig config = new NPMConfig(id, "", "", "", "myprovider", null);
-        assertEquals(id, config.id);
-        assertNull(config.name);
-        assertNull(config.comment);
-        assertNull(config.content);
-        assertNotNull(config.getRegistries());
-    }
-
-    @Test
-    public void test_too_many_global_registries() throws Exception {
-        NPMRegistry privateRegistry = new NPMRegistry("https://private.organization.com/", null, null);
-        NPMRegistry officalRegistry = new NPMRegistry("https://registry.npmjs.org/", null, null);
-
-        thrown.expect(VerifyConfigProviderException.class);
-
-        NPMConfig config = new NPMConfig("too_many_registry", null, null, null, "myprovider", Arrays.asList(privateRegistry, officalRegistry));
-        config.doVerify();
     }
 
 }
