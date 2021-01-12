4916d01eda819cbb65fdd2133dc78e8c112b9ae5
niclet <nicolas.creplet@gmail.com>
2021-01-12 14:20:18 +0100

    Add AIX support (fix)

--- src/main/java/jenkins/plugins/nodejs/tools/pathresolvers/LatestInstallerPathResolver.java
+++ src/main/java/jenkins/plugins/nodejs/tools/pathresolvers/LatestInstallerPathResolver.java
@@ -98,16 +98,14 @@ public class LatestInstallerPathResolver implements InstallerPathResolver {
         case i386:
             if (platform == Platform.OSX && nodeVersion.compareTo(new NodeJSVersion(4, 0, 0)) >= 0 //
                     || ((platform == Platform.SUNOS || platform == Platform.LINUX)
-                            && nodeVersion.compareTo(new NodeJSVersion(10, 0, 0)) >= 0) //
-                    || platform == Platform.AIX) {
+                            && nodeVersion.compareTo(new NodeJSVersion(10, 0, 0)) >= 0)) {
                 throw new IllegalArgumentException(Messages.InstallerPathResolver_unsupportedArch(version, cpu.name(), platform.name()));
             }
             arch = "x86";
             break;
         case amd64:
             if (platform == Platform.SUNOS && //
-                    (new NodeJSVersionRange("[7, 7.5)").includes(nodeVersion) || nodeVersion.compareTo(new NodeJSVersion(0, 12, 18)) == 0) //
-                    || platform == Platform.AIX) {
+                    (new NodeJSVersionRange("[7, 7.5)").includes(nodeVersion) || nodeVersion.compareTo(new NodeJSVersion(0, 12, 18)) == 0)) {
                 throw new IllegalArgumentException(Messages.InstallerPathResolver_unsupportedArch(version, cpu.name(), platform.name()));
             }
             if (isMSI && nodeVersion.compareTo(new NodeJSVersion(4, 0, 0)) < 0) {
@@ -117,13 +115,11 @@ public class LatestInstallerPathResolver implements InstallerPathResolver {
             break;
         case arm64:
         case armv6l:
-            if (nodeVersion.compareTo(new NodeJSVersion(12, 0, 0)) >= 0 || nodeVersion.compareTo(new NodeJSVersion(8, 6, 0)) == 0 //
-                || platform == Platform.AIX) {
+            if (nodeVersion.compareTo(new NodeJSVersion(12, 0, 0)) >= 0 || nodeVersion.compareTo(new NodeJSVersion(8, 6, 0)) == 0) {
                 throw new IllegalArgumentException(Messages.InstallerPathResolver_unsupportedArch(version, cpu.name(), platform.name()));
             }
         case armv7l:
-            if (nodeVersion.compareTo(new NodeJSVersion(4, 0, 0)) < 0 //
-                || platform == Platform.AIX) {
+            if (nodeVersion.compareTo(new NodeJSVersion(4, 0, 0)) < 0) {
                 throw new IllegalArgumentException(Messages.InstallerPathResolver_unsupportedArch(version, cpu.name(), platform.name()));
             }
             arch = cpu.name();
